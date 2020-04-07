f4b3b8b69896fea1d28d116d9f39d346c34c4b86
Younes Haddam <48913541+Younext19@users.noreply.github.com>
2020-04-08 01:56:04 +0200

    Added Login/Signup
    Login.php and signup.php

---
+++ Signup.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Enter Email" name="email" required>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+        <label for="psw-repeat"><b>Repeat Password</b></label>
+        <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+        
+        <button type="submit" class="signup">Sign Up</button><input type="submit">
+    </form>
+</body>
+</html>
