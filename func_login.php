6f72468a1b3af63176b7d064824b244cff8f7f0f
iliesOuldmenouer <63064471+iliesOuldmenouer@users.noreply.github.com>
2020-04-08 18:49:45 +0100

    function to login

---
+++ func_login.php
@@ -0,0 +1,37 @@
+<?php
+
+session_start();
+//Conect to database :
+$con = mysqli_connect('localhost', 'root', '');
+mysqli_select_db($con, 'student-application');
+//-------------------
+
+//var initialisation :
+$password = $_POST['password'];
+$email    = $_POST['email'];
+//-------------------
+
+//check if user exist :
+$s      = " select * from students where email ='$email' && password = '$password' ";
+$result = mysqli_query($con, $s);
+$num    = mysqli_num_rows($result);
+if ($num == 1) {
+
+
+
+
+
+header('location:application_submited.php');
+
+
+  //show alert message if user password wrong :
+?>
+<script> alert ("Vous avez entré des informations incorrectes (le compte n'existe pas)");
+
+window.location.replace(\"http://localhost/students-application/index.html\");
+</script>
+
+<?php
+}
+//-------------------
+?>
