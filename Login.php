7d303c274b5c519fd4a96e1c1939cb449d801e4f
Younes Haddam <48913541+Younext19@users.noreply.github.com>
2020-04-08 22:03:42 +0200

    login
    hawadi login

--- Login.php
+++ Login.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
-    
+    <form action="func_login.php" method="post">
+        <label for="email"><b>Email</b></label>
+        <input type="email" placeholder="Enter Email" name="email" required>
+
+        </br>
+
+        <label for="password"><b>Password :</b></label>
+        <input type="password" placeholder="Enter Email" name="password" required>
+        
+        <br>
+        <input type="submit">
+    </form>
 </body>
 </html>
