4ae3d5e4d4aadb6c7ec4c1d8af590ca54bb0a0dc
Younes Haddam <48913541+Younext19@users.noreply.github.com>
2020-04-08 02:45:43 +0200

    SIGNUP
    Form Signup Done

--- Signup.php
+++ Signup.php
@@ -6,16 +6,59 @@
     <title>Document</title>
 </head>
 <body>
-    <form>
+    <form action="func_singup.php" method="post">
+
+        <label for="matricule"><b>Matricule : </b></label>
+        <input type="text" placeholder="Matricule De Attestation De Scolarité" name="matricule" required>
+
+        </br>
+
+        <label for="fullname"><b>Full Name : </b></label>
+        <input type="text" placeholder="Entrez Nom et Prénom" name="full_name" required>
+
+        </br>
+
         <label for="email"><b>Email</b></label>
-        <input type="text" placeholder="Enter Email" name="email" required>
+        <input type="email" placeholder="Enter Email" name="email" required>
 
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
+        </br>
 
-        <label for="psw-repeat"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+        <label for="password"><b>Password :</b></label>
+        <input type="password" placeholder="Enter Email" name="password" required>
+
+        </br>
+
+        <label for="email"><b>Email</b></label>
+        <input type="email" placeholder="Enter Email" name="email" required>
+
+        </br>
+
+        <label for="MI"><b>MI</b></label>
+        <input type="checkbox"  name="filiere" >
+
+        <label for="SM"><b>SM</b></label>
+        <input type="checkbox"  name="filiere" >
+
+        </br>
+
+        <label for="moy_math"><b>Moyenne Unité Math : </b></label>
+        <input type="text" placeholder="Moy Unité Math" name="moiyenne_math" required>
+
+        </br>
+
+        <label for="moy_info"><b>Moyenne Unité Info : </b></label>
+        <input type="text" placeholder="Moy Unité Info" name="moiyenne_info" required>
+
+        </br>
+
+        <label for="Math"><b>Math</b></label>
+        <input type="checkbox"  name="speciality_favorable" >
         
+        <label for="Info"><b>Info</b></label>
+        <input type="checkbox"  name="speciality_favorable" >
+
+        </br>
+
         <button type="submit" class="signup">Sign Up</button><input type="submit">
     </form>
 </body>
