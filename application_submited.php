7078779e92840371227160e0ce0818b98704e12a
iliesOuldmenouer <63064471+iliesOuldmenouer@users.noreply.github.com>
2020-04-08 18:47:59 +0100

    Create application_submited.php

---
+++ application_submited.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>application_submited</title>
+	</head>
+	<body>
+  application submited succesfully !
+		
+	</body>
+</html>
